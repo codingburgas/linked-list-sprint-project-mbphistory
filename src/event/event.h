5b06fa0b55c526c89ba5f16be2e1a98f2720c480
IRGyurov22 <IRGyurov22@codingburgas.bg>
2025-04-05 19:17:55 +0300

    Add event.h and basic event.cpp structure

--- src/event/event.h
+++ src/event/event.h
@@ -1,4 +1,21 @@
 #ifndef EVENT_H
 #define EVENT_H
+#include <string>
 
+struct Event {
+    std::string date;
+    std::string topic;
+    std::string title;
+    std::string location;
+    std::string leader;
+    std::string participants;
+    std::string result;
+    Event* next; // pointer to next node in the linked list
+};
+
+namespace EventList {
+    void init(Event*& head, const Event& newEvent);
+    void addAtBeginning(Event*& head, const Event& newEvent);
+    void addAtEnd(Event*& head, const Event& newEvent);
+}
 #endif //EVENT_H
--- src/event/event_core.cpp
+++ src/event/event_core.cpp
@@ -1,1 +1,30 @@
+#include "event.h"
+#include <iostream>
 
+namespace EventList {
+
+    void init(Event*& head, const Event& newEvent) {
+        head = new Event(newEvent);
+        head->next = nullptr;
+    }
+
+    void addAtBeginning(Event*& head, const Event& newEvent) {
+        Event* newNode = new Event(newEvent);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void addAtEnd(Event*& head, const Event& newEvent) {
+        Event* newNode = new Event(newEvent);
+        newNode->next = nullptr;
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Event* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
